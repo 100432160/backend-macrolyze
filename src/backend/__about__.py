f9de204b1e18499a6a75d117ab6a8679cd322ca2
arturo_cardenal <100432160@alumnos.uc3m.es>
2024-11-24 15:50:22 -0800

    Initial commit

---
+++ src/backend/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present arturo_cardenal <100432160@alumnos.uc3m.es>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present arturo_cardenal <100432160@alumnos.uc3m.es>
+#
+# SPDX-License-Identifier: MIT
