3e891430ee606640247c08f1606ab32641cc3624
arturo_cardenal <100432160@alumnos.uc3m.es>
2024-12-01 07:28:08 -0800

    CRUD meals and meal_foods working

--- src/backend/models/meal.py
+++ src/backend/models/meal.py
@@ -1,3 +1,4 @@
+import uuid
 from sqlalchemy import Column, String, Date, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
@@ -6,7 +7,7 @@ from backend.core.database import Base
 class Meal(Base):
     __tablename__ = "meals"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
     type = Column(String, nullable=False)
     date = Column(Date, nullable=False)
