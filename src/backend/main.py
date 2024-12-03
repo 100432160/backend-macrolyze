2df4ee6d7720bc8a88296c13bc4460de13fb10db
Arturo C <100432160@alumnos.uc3m.es>
2024-12-02 18:30:40 -0800

    resolved cors problem

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from backend.routes import users, foods, meals, food_groups, weekly_tracker
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Macrolyze API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Especifica el dominio del frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
 # Incluir los routers
 app.include_router(users.router)
